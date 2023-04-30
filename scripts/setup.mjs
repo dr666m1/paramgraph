e9161d18bb5a07bb15bc6863310c8732d9e34a17
dr666m1 <skndr666m1@gmail.com>
2023-04-30 15:50:16 +0900

    use zx

---
+++ scripts/setup.mjs
@@ -0,0 +1,6 @@
+#!/usr/bin/env zx
+
+let account = await $`aws sts get-caller-identity --query Account --output text`;
+let region = await $`aws configure get region`;
+
+await $`npx cdk bootstrap aws://${account}/${region}`
