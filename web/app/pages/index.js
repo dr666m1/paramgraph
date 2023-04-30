a8498679d8e5545b96eefa9151fc17c915b4f09a
dr666m1 <skndr666m1@gmail.com>
2023-05-01 00:02:22 +0900

    locate compontents

--- web/app/pages/index.js
+++ web/app/pages/index.js
@@ -6,9 +6,24 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className={inter.className}>
-      <button className="button">
-        {greet()}
-      </button>
+      <nav class="navbar">
+        <div class="navbar-brand">param-graph</div>
+      </nav>
+      <div className="columns">
+        <div className="column is-one-fifth">
+          <div className="box">
+            first column
+          </div>
+          <div className="box">
+            first column
+          </div>
+        </div>
+        <div className="column">
+          <button className="button">
+            {greet()}
+          </button>
+        </div>
+      </div >
     </div >
   )
 }
