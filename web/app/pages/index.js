8d64109135f1e73270840bf741469b5b753461a2
dr666m1 <skndr666m1@gmail.com>
2023-04-30 19:30:46 +0900

    simplify

--- web/app/pages/index.js
+++ web/app/pages/index.js
@@ -1,14 +1,8 @@
-import { Inter } from 'next/font/google'
 import { greet } from "stats"
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div> {greet()} </div>
-    </main>
+    <div> {greet()} </div>
   )
 }
