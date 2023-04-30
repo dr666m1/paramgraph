389d0302d47fd01a1a8d42d7e128826bc03a6e71
dr666m1 <skndr666m1@gmail.com>
2023-04-30 20:02:07 +0900

    tailwind -> bulma

--- web/app/pages/index.js
+++ web/app/pages/index.js
@@ -3,6 +3,10 @@ import { greet } from "stats"
 
 export default function Home() {
   return (
-    <div> {greet()} </div>
+    <div>
+      <button class="button">
+        {greet()}
+      </button>
+    </div>
   )
 }
