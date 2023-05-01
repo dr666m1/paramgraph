3b772601dde1c20eab9bd84f3fe714b2cab71864
dr666m1 <skndr666m1@gmail.com>
2023-05-01 10:20:29 +0900

    deploy static website

--- deployments/lib/paramgraph.js
+++ deployments/lib/paramgraph.js
@@ -1,15 +1,23 @@
 const cdk = require('aws-cdk-lib');
 const s3 = require('aws-cdk-lib/aws-s3');
+const s3deploy = require('aws-cdk-lib/aws-s3-deployment');
 
 class Paramgraph extends cdk.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'MyFirstBucket', {
-      versioned: true,
+    const bucket = new s3.Bucket(this, 'MyFirstBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS, // default seems BLOCK_ALL
     });
+
+    new s3deploy.BucketDeployment(this, "DeployWebSite", {
+      sources: [s3deploy.Source.asset("../web/app/out")],
+      destinationBucket: bucket,
+    })
   }
 }
 
