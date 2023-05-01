738719a760cd6151207d34be8aa003acdc503084
dr666m1 <skndr666m1@gmail.com>
2023-05-01 13:20:10 +0900

    install webpack-license-plugin

--- web/app/next.config.js
+++ web/app/next.config.js
@@ -1,8 +1,12 @@
+const LicensePlugin = require("webpack-license-plugin")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   webpack: (config) => {
+    config.plugins.push(new LicensePlugin())
+
     config.experiments.syncWebAssembly = true
     config.experiments.asyncWebAssembly = true
     return config
