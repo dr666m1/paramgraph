1db371d4c53b0d1cddc241ed6c04d6548a5a343b
dr666m1 <skndr666m1@gmail.com>
2023-05-02 13:15:47 +0900

    tag -> button

--- web/app/pages/notices.js
+++ web/app/pages/notices.js
@@ -2,10 +2,10 @@ export default function Notices() {
   return <>
     <section className="content">
       <h1 className="title">Third Party License Notices</h1>
-      <p>This Web application depends on packages listed in these json files.</p>
+      <p>This web app depends on packages listed in these json files.</p>
       <ul>
-        <li>JavaScript packages <a href="/notices-js.json" className="tag is-primary">download</a></li>
-        <li>Rust packages <a href="/notices-rs.json" className="tag is-primary">download</a></li>
+        <li>JavaScript packages (notices-js.json) <a href="/notices-js.json" className="button is-primary is-small">download</a></li>
+        <li>Rust packages (notices-rs.json) <a href="/notices-rs.json" className="button is-primary is-small">download</a></li>
       </ul>
     </section>
   </>
