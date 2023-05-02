e302a4d676367a4b6b7d781d985441523f4f57a6
dr666m1 <skndr666m1@gmail.com>
2023-05-02 11:55:55 +0900

    add normal function

--- web/stats/src/lib.rs
+++ web/stats/src/lib.rs
@@ -1,16 +1,16 @@
-mod utils;
-
-use statrs::distribution::{ContinuousCDF, Exp};
+use js_sys::Array;
+use statrs::distribution::{Continuous, Normal};
+use std::iter::FromIterator;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn greet() -> String {
-    utils::set_panic_hook();
     "hello stats!".to_string()
 }
 
 #[wasm_bindgen]
-pub fn culc_exp() -> f64 {
-    let n = Exp::new(1.0).unwrap();
-    n.cdf(1.0)
+pub fn normal(from: f64, to: f64, mu: f64, sigma: f64) -> Array {
+    let n = Normal::new(mu, sigma).unwrap();
+    let arr = Array::from_iter([JsValue::from_f64(n.pdf(from)), JsValue::from_f64(n.pdf(to))]);
+    arr
 }
