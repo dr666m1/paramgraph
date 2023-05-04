e212507ce321445238d2b02594dd7416cd7cac23
Dairyo Sekine <d.sekine@cluster.mu>
2023-05-04 22:22:46 +0900

    fix typo

--- web/app/src/constants.js
+++ web/app/src/constants.js
@@ -5,7 +5,7 @@ export const defaultDistribution = { name: "unspecified", parameters: [] }
 export const distributions = [
   defaultDistribution,
   {
-    name: "N(μ, α)",
+    name: "N(μ, σ)",
     parameters: [
       { "key": "μ", "value": 0 },
       { "key": "σ", "value": 1 },
