e52e6470596cb261ec301e90596117f3cf48ef20
Dairyo Sekine <d.sekine@cluster.mu>
2023-05-05 21:51:02 +0900

    enable to update graph, using query string

--- web/app/pages/index.js
+++ web/app/pages/index.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import Chart from "../components/Chart";
 import DistBox from "../components/DistBox";
-import { defaultDistribution } from "../src/distribution";
+import { defaultDistribution, getDistByName } from "../src/distribution";
+import { useRouter } from "next/router";
 
 const newDefaultDataset = (idx) => {
   return {
@@ -14,10 +15,28 @@ const newDefaultDataset = (idx) => {
 
 export default function Home() {
   const [datasets, setDatasets] = useState([newDefaultDataset(0)]);
+  const router = useRouter();
   useEffect(() => {
     const height = document.getElementById("chart").offsetHeight;
     document.getElementById("right-column").style.maxHeight = `${height}px`;
-  });
+
+    // https://hkc7180.medium.com/how-handle-query-in-nextjs-router-62abb1927c1d
+    if (router.query.id) {
+      const nord = getDistByName("N(μ, σ)");
+      router.replace({ query: {} });
+      setDatasets([
+        {
+          label: nord.name,
+          showLine: true,
+          data: nord.func(-3, 3, nord.parameters),
+          idx: 0,
+        },
+      ]);
+    }
+    console.log(
+      "if you see this message thousands of times, something is going wrong"
+    );
+  }, [router.query]);
 
   return (
     <div className="columns">
