9336fec52a2f16322db23e9cb0432dad03b2a8d2
dr666m1 <skndr666m1@gmail.com>
2023-05-06 22:11:06 +0900

    route53, cloudfront

--- deployments/lib/paramgraph.js
+++ deployments/lib/paramgraph.js
@@ -1,6 +1,9 @@
 const cdk = require("aws-cdk-lib");
 const s3 = require("aws-cdk-lib/aws-s3");
 const s3deploy = require("aws-cdk-lib/aws-s3-deployment");
+const cloudfront = require("aws-cdk-lib/aws-cloudfront");
+const origins = require("@aws-cdk/aws-cloudfront-origins");
+const route53 = require("aws-cdk-lib/aws-route53");
 
 class Paramgraph extends cdk.Stack {
   constructor(scope, id, props) {
@@ -17,6 +20,18 @@ class Paramgraph extends cdk.Stack {
     new s3deploy.BucketDeployment(this, "DeployWebSite", {
       sources: [s3deploy.Source.asset("../web/app/out")],
       destinationBucket: bucket,
+    });
+
+    // NOTE you havet to add NS recort to dr666m1.net host zone
+    new route53.PublicHostedZone(this, "HostZone", {
+      zoneName: "p2g.dr666m1.net",
+    });
+
+    new cloudfront.Distribution(this, "MyDist", {
+      defaultBehavior: {
+        origin: new origins.S3Origin(bucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
     });
   }
 }
