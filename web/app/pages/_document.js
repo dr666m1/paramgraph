43d238619b7eb7d18d4de9c75f0ab4700edec4d0
dr666m1 <skndr666m1@gmail.com>
2023-05-07 17:57:06 +0900

    add favicon.ico

--- web/app/pages/_document.js
+++ web/app/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
+      <link rel="icon" href="/favicon.ico" />
       <title>paramgraph</title>
       <body>
         <Main />
