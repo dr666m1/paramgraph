d4fc87c413f17a8a2dd9271419b638660e9284df
dr666m1 <skndr666m1@gmail.com>
2023-05-28 14:47:10 +0900

    add test case

--- web/app/__tests__/app.test.tsx
+++ web/app/__tests__/app.test.tsx
@@ -1,6 +1,11 @@
 import { render } from "@testing-library/react";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 test("NavBar", () => {
   render(<NavBar />);
+});
+
+test("Footer", () => {
+  render(<Footer />);
 });
