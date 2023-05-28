9d13d23ed8de9813f0235158cbfbf514925d01b8
dr666m1 <skndr666m1@gmail.com>
2023-05-28 12:18:32 +0900

    fix test

--- deployments/test/lib.test.ts
+++ deployments/test/lib.test.ts
@@ -1,6 +1,14 @@
 import * as cdk from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
 import { Paramgraph } from "../lib/paramgraph";
+import * as fs from "fs";
+
+beforeAll(() => {
+  const dir = "../web/app/out";
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+});
 
 test("first test", () => {
   const app = new cdk.App();
